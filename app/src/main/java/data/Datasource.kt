dc75173940ff9eaae27667a8ac519fa2d20ee733
aisyaaahm25 <aisyaaahm2506@gmail.com>
2024-10-25 22:00:31 +0700

    Initial commit

---
+++ app/src/main/java/data/Datasource.kt
@@ -0,0 +1,14 @@
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            // Tambahkan afirmasi lain sesuai kebutuhan
+        )
+    }
+}
